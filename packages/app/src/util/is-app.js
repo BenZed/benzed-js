33def75da6162182f99c923d4cd016e825221b70
BenZed <bengaumond@gmail.com>
2019-01-14 06:31:49 -0800

    is.object
    doenst work on heroku, for some reason

--- packages/app/src/util/is-app.js
+++ packages/app/src/util/is-app.js
@@ -16,7 +16,12 @@ function isApp (app) {
   if (this !== undefined)
     app = this
 
-  return is.object(app) && METHODS.every(method => is.func(app[method]))
+  // TODO is.object doesn't seem to work on heroku, so we're doing this for now
+  const type = typeof app
+
+  return app !== null &&
+    (type === 'object' || type === 'function') &&
+    METHODS.every(method => is.func(app[method]))
 
 }
 
